f8281187e3e34b111f752d52819e46e8183dd703
Thurston <dalythu@gmail.com>
2022-12-30 16:03:47 -0500

    Initial Solution to Practice JS

--- Treehouse/Practice js if then/js/math.js
+++ Treehouse/Practice js if then/js/math.js
@@ -1,13 +1,27 @@
 // declare program variables
-
+let num1;
+let num2;
 // announce the program
 alert("Let's do some math!");
 
 // collect numeric input
-var num1 = prompt("Please type a number");
+num1 = prompt("Please type a number");
 num1 = parseFloat(num1);
-var num2 = prompt("Please type another number");
+num2 = prompt("Please type another number");
 num2 = parseFloat(num2);
+
+if ( num2 === 0 ) {
+    alert("The second number is 0. You can't divide by zero. Reload and try again.");
+    num2 = prompt("Please type another number");
+    num2 = parseFloat(num2);
+    
+} else if ( isNaN(num1) || isNaN(num2) ) {
+    alert("At least one of the values you typed is not a number. Reload and try again.");
+    num1 = prompt("Please type a number");
+    num1 = parseFloat(num1);
+    num2 = prompt("Please type another number");
+    num2 = parseFloat(num2);
+} else {
 
 // build an HTML message
 var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
@@ -22,3 +36,17 @@ message += num1 + " - " + num2 + " = " + (num1 - num2);
 // write message to web page
 document.write(message);
 
+}
+
+// build an HTML message
+var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
+message += num1 + " + " + num2 + " = " + (num1 + num2);
+message += "<br>";
+message += num1 + " * " + num2 + " = " + (num1 * num2);
+message += "<br>";
+message += num1 + " / " + num2 + " = " + (num1 / num2);
+message += "<br>";
+message += num1 + " - " + num2 + " = " + (num1 - num2);
+  
+// write message to web page
+document.write(message);
