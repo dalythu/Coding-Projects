9ff4c4cdedb4c64f37d823d3bdab6837d68771c9
Thurston <dalythu@gmail.com>
2023-01-10 18:30:33 -0500

    corrected random number generated

--- Treehouse/Random Number Challenge/js/random.js
+++ Treehouse/Random Number Challenge/js/random.js
@@ -5,7 +5,7 @@ let userInput = prompt(`Enter a number`);
 userInput = parseInt(userInput);
 
 // Use Math.random() and the user's number to generate a random number
-let randomNumber = Math.floor(Math.random(userInput) * 6) + 1;
+let randomNumber = Math.floor(Math.random() * userInput) + 1;
 
 // Create a message displaying the random number
 let message = `<h2>The random number you generated is ${randomNumber}</h2>`
