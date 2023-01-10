b061443b9e11192b69be5d0515b6b1a6e16d7f21
Thurston <dalythu@gmail.com>
2023-01-10 18:58:44 -0500

    second solution selecting between two numbers

--- Treehouse/Random Number Challenge/js/random.js
+++ Treehouse/Random Number Challenge/js/random.js
@@ -1,13 +1,42 @@
+/*
 // Collect input from a user
 let userInput = prompt(`Enter a number`);
 
 // Convert the input to a number
 userInput = parseInt(userInput);
 
-// Use Math.random() and the user's number to generate a random number
-let randomNumber = Math.floor(Math.random() * userInput) + 1;
 
-// Create a message displaying the random number
-let message = `<h2>The random number you generated is ${randomNumber}</h2>`
 
-document.querySelector('main').innerHTML = message; 
+if (userInput) {
+    // Use Math.random() and the user's number to generate a random number
+    let randomNumber = Math.floor(Math.random() * userInput) + 1;
+
+    // Create a message displaying the random number
+    let message = `<h2>The random number you generated is ${randomNumber}</h2>`
+
+    document.querySelector('main').innerHTML = message; 
+} else {
+    alert(`Please enter a number and not text`)
+};
+*/
+
+// Second Code Challenge
+// Collect input from a user
+const userFirstInput = prompt(`Enter a number`);
+const userSecondInput = prompt(`Enter a second number`);
+
+// Convert the input to a number
+firstNumber = parseInt(userFirstInput);
+secondNumber = parseInt(userSecondInput);
+
+if (firstNumber || secondNumber) {
+    // Use Math.random() and the user's number to generate a random number
+    let randomNumber = Math.floor(Math.random() * (secondNumber - firstNumber)) + firstNumber;
+
+    // Create a message displaying the random number
+    let message = `<h2>${randomNumber} is a number between ${firstNumber} and ${secondNumber}</h2>`
+
+    document.querySelector('main').innerHTML = message; 
+} else {
+    alert(`Please enter two number and try again`)
+};
